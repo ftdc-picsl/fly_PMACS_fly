58aa85ae917630c4ff5eaa5ea873297c2dd1d584
Christopher Olm <caugolm@gmail.com>
2026-03-31 08:47:54 -0400

    datatype asl -> perf for proper bids of ASL

--- add_intendedfor_dwi.py
+++ add_intendedfor_dwi.py
@@ -10,15 +10,15 @@ import os
 import sys
 import glob
 
-sub = str(sys.argv[1]) #sub-AAA
-ses = str(sys.argv[2]) #ses-YYY
+sub = str(sys.argv[1])
+ses = str(sys.argv[2]) 
 bids_base = str(sys.argv[3])
 
 dwi_dir = "{}/sub-{}/ses-{}/dwi/".format(bids_base,sub,ses)
 print("dwi_dir: ", dwi_dir)
 dwi_file = os.path.basename(glob.glob(dwi_dir+"*.nii.gz")[0])
 fmap_dir = "{}/sub-{}/ses-{}/fmap/".format(bids_base,sub,ses)
-json_file = glob.glob(fmap_dir+"/*.json")[0]
+json_file = glob.glob(fmap_dir+"/*epi.json")[0]
 print("json_file: ", json_file)
 with open(json_file,'r') as f:
     data=json.load(f)
